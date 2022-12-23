3b2c9ac2147f2884556953c55e9a41045fa79fd5
Jean Gabriel <jeangabrieljb9@gmail.com>
2022-12-23 14:45:15 -0300

    criação do module de pesquisa

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
+import { PokeSearchComponent } from './poke-search/poke-search.component';
 
 @NgModule({
   declarations: [
-    PokeHeaderComponent
+    PokeHeaderComponent,
+    PokeSearchComponent
   ],
   exports: [
-    PokeHeaderComponent
+    PokeHeaderComponent,
+    PokeSearchComponent
   ],
   imports: [
     CommonModule
