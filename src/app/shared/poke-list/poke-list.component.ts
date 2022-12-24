e4ac3bb18c1fa4f255f311444c17175369eacd9c
Jean Gabriel <jeangabrieljb9@gmail.com>
2022-12-24 01:14:01 -0300

    implementando a logica do search

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokeListComponent implements OnInit {
 
+  private setAllPokemons: any;
   public getAllPokemons: any;
 
   constructor(private pokeApiService: PokeApiService) { }
@@ -15,10 +16,18 @@ export class PokeListComponent implements OnInit {
   ngOnInit(): void {
     this.pokeApiService.apiListAllPokemons.subscribe(
       res => {
-        this.getAllPokemons = res.results;
-        console.log(this.getAllPokemons);
+        this.setAllPokemons = res.results;
+        this.getAllPokemons = this.setAllPokemons;
       }
     );
+  }
+
+  public getSearch(value: string){
+    const filter = this.setAllPokemons.filter((res: any) =>{
+      return !res.name.indexOf(value.toLowerCase());
+    });
+
+    this.getAllPokemons = filter;
   }
 
 }
