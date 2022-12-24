37910efc5caaa84e85144d9a032802c01304ffde
Jean Gabriel <jeangabrieljb9@gmail.com>
2022-12-24 13:58:12 -0300

    exibindo pokemons e direcionando para pagina seguinte

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin, Subscriber } from 'rxjs';
+
+import { PokeApiService } from './../../service/poke-api.service';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon'
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species'
+
+  public pokemon: any;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private pokeApiService: PokeApiService
+    ) { }
 
   ngOnInit(): void {
+    this.getPokemon;
+  }
+
+  get getPokemon(){
+    const id = this.activatedRoute.snapshot.params["id"];
+    const pokemon = this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
+    const name = this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
+
+    return forkJoin([pokemon, name]).subscribe(
+      res => {
+        this.pokemon = res;
+      }
+    )
   }
 
 }
